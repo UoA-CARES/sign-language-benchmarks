120839b8c7554a96c3d7c66d0c58cb39b348396b
sttaseen <sttaseen@example.com>
2022-12-10 17:53:56 +1300

    feat: add i3d_wlasl

--- i3d_wlasl.py
+++ i3d_wlasl.py
@@ -1,20 +1,28 @@
-_base_ = 'mmaction2/configs/_base_/models/c3d_sports1m_pretrained.py'
+_base_ = [
+    'mmaction2/configs/_base_/models/i3d_r50.py', 'mmaction2/configs/_base_/schedules/sgd_100e.py',
+    'mmaction2/configs/_base_/default_runtime.py'
+]
 
 # dataset settings
 dataset_type = 'RawframeDataset'
 data_root = 'data/wlasl/rawframes'
 data_root_val = 'data/wlasl/rawframes'
-split = 1  # official train/test splits. valid numbers: 1, 2, 3
-clip_length = 16
 ann_file_train = 'data/wlasl/train_annotations.txt'
 ann_file_val = 'data/wlasl/val_annotations.txt'
 ann_file_test = 'data/wlasl/test_annotations.txt'
-img_norm_cfg = dict(mean=[104, 117, 128], std=[1, 1, 1], to_bgr=False)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
-    dict(type='SampleFrames', clip_len=16, frame_interval=1, num_clips=1),
+    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
     dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(128, 171)),
-    dict(type='RandomCrop', size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(
+        type='MultiScaleCrop',
+        input_size=224,
+        scales=(1, 0.8),
+        random_crop=False,
+        max_wh_scale_gap=0),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
@@ -24,35 +32,35 @@ train_pipeline = [
 val_pipeline = [
     dict(
         type='SampleFrames',
-        clip_len=16,
-        frame_interval=1,
+        clip_len=32,
+        frame_interval=2,
         num_clips=1,
         test_mode=True),
     dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(128, 171)),
-    dict(type='CenterCrop', crop_size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'label'])
+    dict(type='ToTensor', keys=['imgs'])
 ]
 test_pipeline = [
     dict(
         type='SampleFrames',
-        clip_len=16,
-        frame_interval=1,
+        clip_len=32,
+        frame_interval=2,
         num_clips=10,
         test_mode=True),
     dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(128, 171)),
-    dict(type='CenterCrop', crop_size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='ThreeCrop', crop_size=256),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'label'])
+    dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=16,
+    videos_per_gpu=1, #8
     workers_per_gpu=2,
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
@@ -67,44 +75,12 @@ data = dict(
         pipeline=val_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=ann_file_test,
+        ann_file=ann_file_val,
         data_prefix=data_root_val,
         pipeline=test_pipeline))
-# optimizer
-optimizer = dict(
-    type='SGD', lr=0.001, momentum=0.9,
-    weight_decay=0.0005)  # this lr is used for 8 gpus
-optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
-# learning policy
-lr_config = dict(policy='step', step=[20, 40])
-total_epochs = 45
-checkpoint_config = dict(interval=5)
 evaluation = dict(
     interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
 
-# WandB setup
-log_config = dict(interval=10, hooks=[
-    dict(type='TextLoggerHook'),
-    dict(type='WandbLoggerHook',
-         init_kwargs={
-             'entity': "cares",
-             'project': "wlasl-100-c3d"
-         },
-         log_artifact=True)
-]
-)
-
-# log_config = dict(
-#     interval=20,
-#     hooks=[
-#         dict(type='TextLoggerHook'),
-#         # dict(type='TensorboardLoggerHook'),
-#     ])
-
 # runtime settings
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-work_dir = f'./work_dirs/c3d_wlasl_{split}_rgb/'
-load_from = None
-resume_from = None
-workflow = [('train', 1)]
+checkpoint_config = dict(interval=5)
+work_dir = './work_dirs/i3d_r50_32x2x1_100e_kinetics400_rgb/'
