0063131d327fb440a81c89184073c02d8248c0b5
sttaseen <sttaseen@gmail.com>
2022-12-15 21:11:27 +1300

    feat: add auto_train.sh

---
+++ auto_train.sh
@@ -0,0 +1,3 @@
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_base_wlasl10_rgb.py --validate --seed 0 --deterministic --gpus 1
+
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_temporalflip_wlasl10_rgb.py --validate --seed 0 --deterministic --gpus 1
