4f9a2d2d8a8214a6265e8ca12f15d6aac915e929
sttaseen <sttaseen@example.com>
2022-12-15 21:06:33 +1300

    feat: add temporal flip

--- experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_temporalflip_wlasl10_rgb.py
+++ experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_temporalflip_wlasl10_rgb.py
@@ -2,42 +2,52 @@
 model = dict(
     type='Recognizer3D',
     backbone=dict(
-        type='C3D',
-        pretrained=  # noqa: E251
-        'https://download.openmmlab.com/mmaction/recognition/c3d/c3d_sports1m_pretrain_20201016-dcc47ddc.pth',  # noqa: E501
-        style='pytorch',
+        type='ResNet3d',
+        pretrained2d=True,
+        pretrained='./work_dirs/i3d_r50_32x2x1_100e_kinetics400_base_rgb/latest.pth'
+        depth=50,
+        conv1_kernel=(5, 7, 7),
+        conv1_stride_t=2,
+        pool1_stride_t=2,
         conv_cfg=dict(type='Conv3d'),
-        norm_cfg=None,
-        act_cfg=dict(type='ReLU'),
-        dropout_ratio=0.5,
-        init_std=0.005),
+        norm_eval=False,
+        inflate=((1, 1, 1), (1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (0, 1, 0)),
+        zero_init_residual=False),
     cls_head=dict(
         type='I3DHead',
-        num_classes=101,
-        in_channels=4096,
-        spatial_type=None,
+        num_classes=400,
+        in_channels=2048,
+        spatial_type='avg',
         dropout_ratio=0.5,
         init_std=0.01),
     # model training and testing settings
     train_cfg=None,
-    test_cfg=dict(average_clips='score'))
+    test_cfg=dict(average_clips='prob'))
+
+# This setting refers to https://github.com/open-mmlab/mmaction/blob/master/mmaction/models/tenons/backbones/resnet_i3d.py#L329-L332  # noqa: E501
 
 
 # dataset settings
 dataset_type = 'RawframeDataset'
 data_root = 'data/wlasl/rawframes'
 data_root_val = 'data/wlasl/rawframes'
-split = 1  # official train/test splits. valid numbers: 1, 2, 3
-clip_length = 16
 ann_file_train = 'data/wlasl/train_annotations.txt'
 ann_file_val = 'data/wlasl/val_annotations.txt'
 ann_file_test = 'data/wlasl/test_annotations.txt'
-img_norm_cfg = dict(mean=[104, 117, 128], std=[1, 1, 1], to_bgr=False)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
-    dict(type='SampleFrames', clip_len=16, frame_interval=1, num_clips=1),
+    dict(type='TemporalFlip')
+    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
     dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(128, 171)),
-    dict(type='RandomCrop', size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(
+        type='MultiScaleCrop',
+        input_size=224,
+        scales=(1, 0.8),
+        random_crop=False,
+        max_wh_scale_gap=0),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
@@ -47,35 +57,35 @@ train_pipeline = [
 val_pipeline = [
     dict(
         type='SampleFrames',
-        clip_len=16,
-        frame_interval=1,
+        clip_len=32,
+        frame_interval=2,
         num_clips=1,
         test_mode=True),
     dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(128, 171)),
-    dict(type='CenterCrop', crop_size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'label'])
+    dict(type='ToTensor', keys=['imgs'])
 ]
 test_pipeline = [
     dict(
         type='SampleFrames',
-        clip_len=16,
-        frame_interval=1,
+        clip_len=32,
+        frame_interval=2,
         num_clips=10,
         test_mode=True),
     dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(128, 171)),
-    dict(type='CenterCrop', crop_size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='ThreeCrop', crop_size=256),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'label'])
+    dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=16,
+    videos_per_gpu=8,
     workers_per_gpu=2,
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
@@ -90,20 +100,23 @@ data = dict(
         pipeline=val_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=ann_file_test,
+        ann_file=ann_file_val,
         data_prefix=data_root_val,
         pipeline=test_pipeline))
-# optimizer
-optimizer = dict(
-    type='SGD', lr=0.001, momentum=0.9,
-    weight_decay=0.0005)  # this lr is used for 8 gpus
-optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
-# learning policy
-lr_config = dict(policy='step', step=[20, 40])
-total_epochs = 1
-checkpoint_config = dict(interval=5)
 evaluation = dict(
     interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
+
+# runtime settings
+checkpoint_config = dict(interval=5)
+work_dir = './work_dirs/i3d_r50_32x2x1_100e_kinetics400_tempflip_rgb/'
+
+checkpoint_config = dict(interval=1)
+log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook'),
+    ])
 
 # # WandB setup
 # log_config = dict(interval=10, hooks=[
@@ -113,23 +126,20 @@ evaluation = dict(
 #              'entity': "cares",
 #              'project': "wlasl-100",
 #              'group': "augmentations",
-#              'name': 'base'
+#              'name': 'temporalflip'
 #          },
 #          log_artifact=True)
 # ]
 # )
 
-log_config = dict(
-    interval=20,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook'),
-    ])
-
 # runtime settings
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = f'./experiments/augmentations/work_dirs/c3d_augments_wlasl100/'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
