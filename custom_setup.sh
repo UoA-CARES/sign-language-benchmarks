b230a746980d26185317303577b8a90c38724586
sttaseen <sttaseen@gmail.com>
2022-12-20 21:12:09 +1300

    chore: add docs for bash scripts

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Set the number of classes (chooses the top_n classes with the most samples)
-n=10
+# Set parameters
+n_classes=10 # classes with the most samples are favoured to create WLASL_{n_classes}
+n_workers=14
 
 # Download the dataset
 mkdir -p data
@@ -14,17 +15,17 @@ unzip wlasl-processed.zip -d ./wlasl-uncompressed
 cd ../..
 
 # Delete missing clips and create a clean annotations file
-python tools/wlasl/create_json.py $n data/wlasl/wlasl-uncompressed
+python tools/wlasl/create_json.py $n_classes data/wlasl/wlasl-uncompressed
 
 # Split the videos
-python tools/wlasl/split_videos.py "wlasl_${n}.json" data/wlasl/wlasl-uncompressed
+python tools/wlasl/split_videos.py "wlasl_${n_classes}.json" data/wlasl/wlasl-uncompressed
 
 # Extra raw frames
-python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-uncompressed/test data/wlasl/rawframes/test --ext mp4 --task rgb --level 1 --num-worker 2 --out-format jpg --use-opencv
-python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-uncompressed/train data/wlasl/rawframes/train --ext mp4 --task rgb --level 1 --num-worker 2 --out-format jpg --use-opencv
-python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-uncompressed/val data/wlasl/rawframes/val --ext mp4 --task rgb --level 1 --num-worker 2 --out-format jpg --use-opencv
+python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-uncompressed/test data/wlasl/rawframes/test --ext mp4 --task rgb --level 1 --new-width 1920 --new-height 1080 --num-worker $n_workers --out-format jpg --use-opencv
+python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-uncompressed/train data/wlasl/rawframes/train --ext mp4 --task rgb --level 1 --new-width 1920 --new-height 1080 --num-worker $n_workers --out-format jpg --use-opencv
+python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-uncompressed/val data/wlasl/rawframes/val --ext mp4 --task rgb --level 1 --new-width 1920 --new-height 1080 --num-worker $n_workers --out-format jpg --use-opencv
 
 # Build the labels
-python tools/wlasl/build_labels.py "wlasl_${n}.json" data/wlasl
+python tools/wlasl/build_labels.py "data/wlasl/wlasl-uncompressed/wlasl_${n_classes}.json" data/wlasl
 
 
--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Set parameters
 json="nslt_100.json"
-num_workers=12
+n_workers=12
 
 # Download the dataset
 mkdir -p data
@@ -16,9 +17,9 @@ cd ../..
 python tools/wlasl/split_videos.py $json data/wlasl/wlasl-complete
 
 # Extra raw frames
-python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-complete/test data/wlasl/rawframes/test --ext mp4 --task rgb --level 1 --num-worker $num_workers --out-format jpg --use-opencv
-python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-complete/train data/wlasl/rawframes/train --ext mp4 --task rgb --level 1 --num-worker $num_workers --out-format jpg --use-opencv
-python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-complete/val data/wlasl/rawframes/val --ext mp4 --task rgb --level 1 --num-worker $num_workers --out-format jpg --use-opencv
+python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-complete/test data/wlasl/rawframes/test --ext mp4 --task rgb --level 1 --num-worker $n_workers --out-format jpg --use-opencv
+python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-complete/train data/wlasl/rawframes/train --ext mp4 --task rgb --level 1 --num-worker $n_workers --out-format jpg --use-opencv
+python mmaction2/tools/data/build_rawframes.py data/wlasl/wlasl-complete/val data/wlasl/rawframes/val --ext mp4 --task rgb --level 1 --num-worker $n_workers --out-format jpg --use-opencv
 
 # Build the labels
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
