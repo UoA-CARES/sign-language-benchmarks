7e8b170f5c6e5408ca3e96189a53bd54a99b1a97
sttaseen <sttaseen@gmail.com>
2022-12-21 21:11:44 +1300

    test: add no-augment

--- auto_train.sh
+++ auto_train.sh
@@ -1,8 +1,9 @@
-conda activate wlasl
-bash setup.sh
-python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_200e_kinetics400_base_wlasl10_rgb.py --validate --seed 0 --deterministic --gpus 1
-python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_800e_kinetics400_base_wlasl10_rgb.py --validate --seed 0 --deterministic --gpus 1
-python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_randaug_wlasl10_rgb.py --validate --seed 0 --deterministic --gpus 1
-python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_randaug_wlasl10_rgb.py --validate --seed 1 --deterministic --gpus 1
-python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_randaug_wlasl10_rgb.py --validate --seed 2 --deterministic --gpus 1
+# noaug
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_noaug_wlasl100_rgb.py --validate --seed 0 --deterministic --gpus 1
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_noaug_wlasl100_rgb.py --validate --seed 1 --deterministic --gpus 1
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/i3d_r50_32x2x1_100e_kinetics400_noaug_wlasl100_rgb.py --validate --seed 2 --deterministic --gpus 1
+
+
+
+
 
