46d1f05dcb21d7c9de6b1883074f934b7b55d482
sttaseen <sttaseen@gmail.com>
2022-12-21 22:43:00 +1300

    test: add train_randaug.sh

---
+++ train_randaug.sh
@@ -0,0 +1,5 @@
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/randaug/i3d_r50_32x2x1_200e_kinetics400_randaug_wlasl100_rgb_0.py --validate --deterministic --gpus 1 --seed 0
+
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/randaug/i3d_r50_32x2x1_200e_kinetics400_randaug_wlasl100_rgb_1.py --validate --deterministic --gpus 1 --seed 1
+
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/randaug/i3d_r50_32x2x1_200e_kinetics400_randaug_wlasl100_rgb_2.py --validate --deterministic --gpus 1 --seed 2
