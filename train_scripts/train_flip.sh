b25ef817cdb1abf4bd60c1ef907c3ac996df2268
sttaseen <sttaseen@gmail.com>
2022-12-22 12:12:43 +1300

    refactor: move train_scripts to a folder

--- train_scripts/train_flip.sh
+++ train_scripts/train_flip.sh
@@ -1,3 +1,4 @@
+cd ..
 python mmaction2/tools/train.py experiments/augmentations/configs/i3d/flip/i3d_r50_32x2x1_100e_kinetics400_flip_wlasl100_rgb_0.py --validate --deterministic --gpus 1 --seed 0
 
 python mmaction2/tools/train.py experiments/augmentations/configs/i3d/flip/i3d_r50_32x2x1_100e_kinetics400_flip_wlasl100_rgb_1.py --validate --deterministic --gpus 1 --seed 1
