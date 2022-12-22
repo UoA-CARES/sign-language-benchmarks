68c22c604cc53cdc8257f3816da64d254e6763f3
sttaseen <sttaseen@gmail.com>
2022-12-23 10:18:30 +1300

    feat: add mixup

---
+++ train_scripts/train_mixup.sh
@@ -0,0 +1,8 @@
+cd ..
+
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/mixup/i3d_r50_32x2x1_100e_kinetics400_mixup_wlasl100_rgb_0.py --validate --deterministic --gpus 1 --seed 0
+
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/mixup/i3d_r50_32x2x1_100e_kinetics400_mixup_wlasl100_rgb_1.py --validate --deterministic --gpus 1 --seed 1
+
+python mmaction2/tools/train.py experiments/augmentations/configs/i3d/mixup/i3d_r50_32x2x1_100e_kinetics400_mixup_wlasl100_rgb_2.py --validate --deterministic --gpus 1 --seed 2
+
