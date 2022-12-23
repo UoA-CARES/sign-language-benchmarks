e87640bce658c67869518d074e6cd46cbb274add
sttaseen <sttaseen@gmail.com>
2022-12-23 23:41:38 +1300

    feat: add mixup notebook

--- experiments/augmentations/copy_augs.sh
+++ experiments/augmentations/copy_augs.sh
@@ -1,3 +1,4 @@
 cp -f __init__.py ../../mmaction2/mmaction/datasets/pipelines
 cp -f my_pipeline.py ../../mmaction2/mmaction/datasets/pipelines
 cp -f randaugment_t.py ../../mmaction2/mmaction/datasets/pipelines
+# cp -f blending_utils.py ../../mmaction2/mmaction/datasets/pipelines
