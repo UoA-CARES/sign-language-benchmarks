3f9f20dc21c37b469576038485ab15e7da45008d
sttaseen <sttaseen@gmail.com>
2022-12-23 23:55:45 +1300

    test: adjust learning rate randaug_t_mscrop

--- experiments/augmentations/configs/i3d/randaug_t_multiscalecrop/i3d_r50_32x2x1_100e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_1.py
+++ experiments/augmentations/configs/i3d/randaug_t_multiscalecrop/i3d_r50_32x2x1_100e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_1.py
@@ -30,7 +30,7 @@ gpu_ids = range(1)
 # optimizer
 optimizer = dict(
     type='SGD',
-    lr=0.01,  # this lr is used for 8 gpus
+    lr=0.01/8,  # this lr is used for 8 gpus
     momentum=0.9,
     weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
