f0ae127eb48bfbf16d81edc9128924b224ca726f
sttaseen <sttaseen@gmail.com>
2022-12-23 21:25:22 +1300

    test: change i3d augmix config

--- experiments/augmentations/configs/i3d/augmix/i3d_r50_32x2x1_100e_kinetics400_augmix_wlasl100_rgb_2.py
+++ experiments/augmentations/configs/i3d/augmix/i3d_r50_32x2x1_100e_kinetics400_augmix_wlasl100_rgb_2.py
@@ -29,7 +29,7 @@ model = dict(
 # optimizer
 optimizer = dict(
     type='SGD',
-    lr=0.01,  # this lr is used for 8 gpus
+    lr=0.01/8,  # this lr is used for 8 gpus
     momentum=0.9,
     weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
