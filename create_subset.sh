77e8e68f10e6afb6a9bfc5d792eeeaaedce21069
sttaseen <sttaseen@gmail.com>
2023-01-06 18:03:23 +1300

    test: change create_subset

--- create_subset.sh
+++ create_subset.sh
@@ -1,1 +1,8 @@
-python tools/wlasl/create_subset.py nslt_100.json 3 data/wlasl/wlasl-complete nslt_100_3.json
+python tools/wlasl/create_subset.py nslt_100.json 2 data/wlasl/wlasl-complete nslt_100_2.json
+python tools/wlasl/create_subset.py nslt_100.json 4 data/wlasl/wlasl-complete nslt_100_4.json
+python tools/wlasl/create_subset.py nslt_100.json 6 data/wlasl/wlasl-complete nslt_100_6.json
+python tools/wlasl/create_subset.py nslt_100.json 8 data/wlasl/wlasl-complete nslt_100_8.json
+python tools/wlasl/create_subset.py nslt_100.json 10 data/wlasl/wlasl-complete nslt_100_10.json
+python tools/wlasl/create_subset.py nslt_100.json 12 data/wlasl/wlasl-complete nslt_100_12.json
+python tools/wlasl/create_subset.py nslt_100.json 14 data/wlasl/wlasl-complete nslt_100_14.json
+python tools/wlasl/create_subset.py nslt_100.json 15 data/wlasl/wlasl-complete nslt_100_15.json
