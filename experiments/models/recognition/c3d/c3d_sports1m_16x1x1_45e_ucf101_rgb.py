035e6ee7e6999b7d5dede99f71d857652f0266d1
sttaseen <sttaseen@gmail.com>
2023-01-11 09:19:50 +1300

    bug: fix c3d file error

--- experiments/models/recognition/c3d/c3d_sports1m_16x1x1_45e_ucf101_rgb.py
+++ experiments/models/recognition/c3d/c3d_sports1m_16x1x1_45e_ucf101_rgb.py
@@ -54,7 +54,7 @@ val_pipeline = [
         frame_interval=1,
         num_clips=1,
         test_mode=True),
-    dict(type='RawFrameDecode'e=(112, 112)),
+    dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(128, 171)),
     dict(type='CenterCrop', crop_size=112),
     dict(type='Normalize', **img_norm_cfg),
