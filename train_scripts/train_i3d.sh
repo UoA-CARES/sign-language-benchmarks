0bd5aa003245bd55b4f13a098d05d8df116c2de5
sttaseen <sttaseen@gmail.com>
2023-01-10 17:19:19 +1300

    feat: add i3d

---
+++ train_scripts/train_i3d.sh
@@ -0,0 +1,15 @@
+cd ..
+
+python mmaction2/tools/train.py experiments/models/recognition/i3d/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb.py  \
+    --work-dir work_dirs/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb/0 \
+    --validate --seed 0 --deterministic
+
+python mmaction2/tools/train.py experiments/models/recognition/i3d/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb.py  \
+    --work-dir work_dirs/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb/1 \
+    --validate --seed 1 --deterministic
+
+python mmaction2/tools/train.py experiments/models/recognition/i3d/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb.py  \
+    --work-dir work_dirs/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb/2 \
+    --validate --seed 2 --deterministic
+
+
