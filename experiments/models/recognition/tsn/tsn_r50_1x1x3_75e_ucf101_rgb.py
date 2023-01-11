7514fae8ffe0f329efb39bdb9ef9f7df3a214f3c
sttaseen <sttaseen@gmail.com>
2023-01-12 11:33:55 +1300

    feat: add tsn

--- experiments/models/recognition/tsn/tsn_r50_1x1x3_75e_ucf101_rgb.py
+++ experiments/models/recognition/tsn/tsn_r50_1x1x3_75e_ucf101_rgb.py
@@ -1,16 +1,45 @@
-_base_ = ['../../_base_/models/tsn_r50.py', '../../_base_/default_runtime.py']
-
-# model settings
-model = dict(cls_head=dict(num_classes=101, init_std=0.001))
-
-# dataset settings
+model = dict(
+    type='Recognizer2D',
+    backbone=dict(
+        type='ResNet',
+        pretrained='torchvision://resnet50',
+        depth=50,
+        norm_eval=False),
+    cls_head=dict(
+        type='TSNHead',
+        num_classes=101,
+        in_channels=2048,
+        spatial_type='avg',
+        consensus=dict(type='AvgConsensus', dim=1),
+        dropout_ratio=0.4,
+        init_std=0.001),
+    train_cfg=None,
+    test_cfg=dict(average_clips=None))
+checkpoint_config = dict(interval=5)
+log_config = dict(interval=10,
+                  hooks=[
+                      dict(type='TextLoggerHook'),
+                      dict(type='WandbLoggerHook',
+                           init_kwargs={
+                               'entity': "cares",
+                               'project': "wlasl-model-ablation",
+                               'group': "tsn",
+                           },
+                           log_artifact=True)
+                  ])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+opencv_num_threads = 0
+mp_start_method = 'fork'
 dataset_type = 'RawframeDataset'
-data_root = 'data/ucf101/rawframes/'
-data_root_val = 'data/ucf101/rawframes/'
-split = 1  # official train/test splits. valid numbers: 1, 2, 3
-ann_file_train = f'data/ucf101/ucf101_train_split_{split}_rawframes.txt'
-ann_file_val = f'data/ucf101/ucf101_val_split_{split}_rawframes.txt'
-ann_file_test = f'data/ucf101/ucf101_val_split_{split}_rawframes.txt'
+data_root = 'data/wlasl/rawframes'
+data_root_val = 'data/wlasl/rawframes'
+ann_file_train = 'data/wlasl/train_annotations.txt'
+ann_file_val = 'data/wlasl/test_annotations.txt'
+ann_file_test = 'data/wlasl/test_annotations.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
@@ -20,11 +49,16 @@ train_pipeline = [
     dict(type='RandomResizedCrop'),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_bgr=False),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs', 'label'])
 ]
+gpu_ids = range(0, 1)
 val_pipeline = [
     dict(
         type='SampleFrames',
@@ -35,7 +69,11 @@ val_pipeline = [
     dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=256),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_bgr=False),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs'])
@@ -50,42 +88,94 @@ test_pipeline = [
     dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='ThreeCrop', crop_size=256),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_bgr=False),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=32,
+    videos_per_gpu=20,
     workers_per_gpu=2,
     test_dataloader=dict(videos_per_gpu=1),
+    val_dataloader=dict(videos_per_gpu=1),
     train=dict(
-        type=dataset_type,
-        ann_file=ann_file_train,
-        data_prefix=data_root,
-        pipeline=train_pipeline),
+        type='RawframeDataset',
+        ann_file='data/wlasl/train_annotations.txt',
+        data_prefix='data/wlasl/rawframes',
+        pipeline=[
+            dict(
+                type='SampleFrames', clip_len=1, frame_interval=1,
+                num_clips=3),
+            dict(type='RawFrameDecode'),
+            dict(type='Resize', scale=(-1, 256)),
+            dict(type='RandomResizedCrop'),
+            dict(type='Resize', scale=(224, 224), keep_ratio=False),
+            dict(type='Flip', flip_ratio=0.5),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_bgr=False),
+            dict(type='FormatShape', input_format='NCHW'),
+            dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+            dict(type='ToTensor', keys=['imgs', 'label'])
+        ]),
     val=dict(
-        type=dataset_type,
-        ann_file=ann_file_val,
-        data_prefix=data_root_val,
-        pipeline=val_pipeline),
+        type='RawframeDataset',
+        ann_file='data/wlasl/test_annotations.txt',
+        data_prefix='data/wlasl/rawframes',
+        pipeline=[
+            dict(
+                type='SampleFrames',
+                clip_len=1,
+                frame_interval=1,
+                num_clips=3,
+                test_mode=True),
+            dict(type='RawFrameDecode'),
+            dict(type='Resize', scale=(-1, 256)),
+            dict(type='CenterCrop', crop_size=256),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_bgr=False),
+            dict(type='FormatShape', input_format='NCHW'),
+            dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+            dict(type='ToTensor', keys=['imgs'])
+        ]),
     test=dict(
-        type=dataset_type,
-        ann_file=ann_file_test,
-        data_prefix=data_root_val,
-        pipeline=test_pipeline))
+        type='RawframeDataset',
+        ann_file='data/wlasl/test_annotations.txt',
+        data_prefix='data/wlasl/rawframes',
+        pipeline=[
+            dict(
+                type='SampleFrames',
+                clip_len=1,
+                frame_interval=1,
+                num_clips=25,
+                test_mode=True),
+            dict(type='RawFrameDecode'),
+            dict(type='Resize', scale=(-1, 256)),
+            dict(type='ThreeCrop', crop_size=256),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_bgr=False),
+            dict(type='FormatShape', input_format='NCHW'),
+            dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+            dict(type='ToTensor', keys=['imgs'])
+        ]))
 evaluation = dict(
     interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
-
-# optimizer
-optimizer = dict(
-    type='SGD', lr=0.00128, momentum=0.9,
-    weight_decay=0.0005)  # this lr is used for 8 gpus
+optimizer = dict(type='SGD', lr=0.00128, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
-# learning policy
-lr_config = dict(policy='step', step=[])
-total_epochs = 75
-
-# runtime settings
-checkpoint_config = dict(interval=5)
-work_dir = f'./work_dirs/tsn_r50_1x1x3_75e_ucf101_split_{split}_rgb/'
+lr_config = dict(policy='step', step=[40, 80])
+total_epochs = 100
+work_dir = './work_dirs/tsn_r50_1x1x3_75e_ucf101_split_rgb/'
+omnisource = False
+module_hooks = []
