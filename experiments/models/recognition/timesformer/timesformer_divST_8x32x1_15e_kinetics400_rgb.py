70c4d0f3741bce9882a7195a2dccc423a96c77a0
sttaseen <sttaseen@gmail.com>
2023-01-11 23:45:55 +1300

    test: add wandb to timesformer

--- experiments/models/recognition/timesformer/timesformer_divST_8x32x1_15e_kinetics400_rgb.py
+++ experiments/models/recognition/timesformer/timesformer_divST_8x32x1_15e_kinetics400_rgb.py
@@ -1,4 +1,27 @@
-_base_ = ['../../_base_/default_runtime.py']
+checkpoint_config = dict(interval=1)
+log_config = dict(interval=10,
+                  hooks=[
+                      dict(type='TextLoggerHook'),
+                      dict(type='WandbLoggerHook',
+                           init_kwargs={
+                               'entity': "cares",
+                               'project': "wlasl-model-ablation",
+                               'group': "timeSformer",
+                           },
+                           log_artifact=True)
+                  ])
+# runtime settings
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
+
 
 # model settings
 model = dict(
