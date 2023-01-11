86afb1bf0b0106e9ac9dd3c5c1e73d7a647cf12e
oh-hi-marx <flabdula@gmail.com>
2023-01-11 13:31:34 +1300

    feat: add train script

--- train_scripts/train_csn.sh
+++ train_scripts/train_csn.sh
@@ -4,9 +4,9 @@ cd ..
 #     --work-dir work_dirs/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb/0 \
 #     --validate --seed 3 --deterministic
 
-python mmaction2/tools/train.py experiments/models/recognition/csn/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb.py \
-    --work-dir work_dirs/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb/1 \
-    --validate --seed 4 --deterministic
+#python mmaction2/tools/train.py experiments/models/recognition/csn/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb.py \
+#    --work-dir work_dirs/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb/1 \
+#    --validate --seed 4 --deterministic
 
 python mmaction2/tools/train.py experiments/models/recognition/csn/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb.py \
     --work-dir work_dirs/ircsn_ig65m_pretrained_bnfrozen_r50_32x2x1_58e_kinetics400_rgb/2 \
