4d0d4c01f685a90d6cc4d36f5fd0688498113e3c
sttaseen <sttaseen@gmail.com>
2023-01-13 17:43:18 +1300

    bug: fix csn train script

--- train_scripts/experiment_sample_csn.sh
+++ train_scripts/experiment_sample_csn.sh
@@ -25,62 +25,62 @@ bash create_subset.sh
 
 json="nslt_100_1.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_1.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/1.py --deterministic --validate --seed 0
 
 json="nslt_100_2.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_2.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/2.py --deterministic --validate --seed 0
 
 json="nslt_100_3.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_3.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/3.py --deterministic --validate --seed 0
 
 json="nslt_100_4.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_4.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/4.py --deterministic --validate --seed 0
 
 json="nslt_100_5.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_5.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/5.py --deterministic --validate --seed 0
 
 json="nslt_100_6.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_6.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/6.py --deterministic --validate --seed 0
 
 json="nslt_100_7.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_7.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/7.py --deterministic --validate --seed 0
 
 json="nslt_100_8.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_8.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/8.py --deterministic --validate --seed 0
 
 json="nslt_100_9.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_9.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/9.py --deterministic --validate --seed 0
 
 json="nslt_100_10.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_10.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/10.py --deterministic --validate --seed 0
 
 json="nslt_100_11.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_11.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/11.py --deterministic --validate --seed 0
 
 json="nslt_100_12.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_12.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/12.py --deterministic --validate --seed 0
 
 json="nslt_100_13.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_13.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/13.py --deterministic --validate --seed 0
 
 json="nslt_100_14.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_14.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/14.py --deterministic --validate --seed 0
 
 json="nslt_100_15.json"
 python tools/wlasl/build_labels.py "data/wlasl/wlasl-complete/${json}" data/wlasl
-python mmaction2/tools/train.py experiments/sample_size/i3d_r50_32x2x1_50e_kinetics400_randaugt_multiscalecrop_wlasl100_rgb_15.py --deterministic --validate --seed 0
+python mmaction2/tools/train.py experiments/sample_size/csn/15.py --deterministic --validate --seed 0
 
 
