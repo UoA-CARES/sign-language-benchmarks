5cedda946868d4073d57e4d55925678919db97f0
sttaseen <sttaseen@gmail.com>
2023-01-18 19:40:39 +1300

    feat: add symbolic script autsl

---
+++ autsl_setup/symbolic.sh
@@ -0,0 +1,21 @@
+ #!/bin/bash 
+ 
+cd ..
+
+# Change the external drive name here
+EXTERNALDRIVE='Sadat'
+
+# Set up the external drive with the following structure
+: '
+EXTERNALDRIVE
+├── autsl
+│   └── data
+└── ...
+ '
+mkdir -p "/media/${USER}/${EXTERNALDRIVE}/autsl"
+mkdir -p "/media/${USER}/${EXTERNALDRIVE}/autsl/data"
+
+# Create the symbolic link
+ln -s "/media/${USER}/${EXTERNALDRIVE}/autsl/data" data
+
+
