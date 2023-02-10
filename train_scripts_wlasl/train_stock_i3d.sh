51a8eac240f750f443b8805a20160cba900cbfd3
sttaseen <sttaseen@gmail.com>
2023-02-11 11:36:44 +1300

    bug: fix i3d

--- train_scripts_wlasl/train_stock_i3d.sh
+++ train_scripts_wlasl/train_stock_i3d.sh
@@ -1,7 +1,7 @@
 cd ..
 
-python mmaction2/tools/train.py mmaction2/configs/recognition/i3d/i3d_nl_dot_product_r50_32x2x1_100e_kinetics400_rgb.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/wlasl/augmentations/configs/i3d/stock/i3d_r50_32x2x1_100e_kinetics400_stock_wlasl100_rgb_0.py --deterministic --seed 0 --validate
 
-python mmaction2/tools/train.py mmaction2/configs/recognition/i3d/i3d_nl_dot_product_r50_32x2x1_100e_kinetics400_rgb.py --validate --deterministic --seed 1
+python mmaction2/tools/train.py experiments/wlasl/augmentations/configs/i3d/stock/i3d_r50_32x2x1_100e_kinetics400_stock_wlasl100_rgb_0.py --deterministic --seed 1 --validate
     
-python mmaction2/tools/train.py mmaction2/configs/recognition/i3d/i3d_nl_dot_product_r50_32x2x1_100e_kinetics400_rgb.py --validate --deterministic --seed 2
+python mmaction2/tools/train.py experiments/wlasl/augmentations/configs/i3d/stock/i3d_r50_32x2x1_100e_kinetics400_stock_wlasl100_rgb_0.py --deterministic --seed 2 --validate
