280ad38a2367b1823c87d69463d6b251ce9d4ccb
sttaseen <sttaseen@gmail.com>
2023-02-22 00:53:31 +1300

    bug: fix wlasl setup

--- setup/wlasl/multimodal/download_preprocessed.sh
+++ setup/wlasl/multimodal/download_preprocessed.sh
@@ -6,8 +6,6 @@ cd ../../..
 # Download the dataset
 mkdir -p data
 cd data
-mkdir -p wlasl
-cd wlasl
 kaggle datasets download -d sttaseen/wlasl2000-seven-sees
 unzip wlasl2000-seven-sees.zip -d ./
 cd ../..
