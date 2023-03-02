98f97e1a49d9b3042109eeaed8aa5cc4c226d2ff
sttaseen <sttaseen@gmail.com>
2023-03-02 23:16:19 +1300

    test: update auto.sh

--- experiments/wlasl/seven-sees/auto.sh
+++ experiments/wlasl/seven-sees/auto.sh
@@ -1,4 +1,7 @@
-python wlasl2000
+python wlasl2000.py
 bash setup.sh
+cd ../../../data/wlasl
+cat train_annotations.txt val_annotations.txt > train_annotations.txt
+cd ../../experiments/wlasl/seven-sees
 python rgb_only.py
 python flow_only.py
